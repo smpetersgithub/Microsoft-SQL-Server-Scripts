b51263af01f72cfc82f9a1aa25c93acc74ae2cdf
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-10-10 10:26:32 -0500

    Update Stored Procedure Definition.sql

--- Scripts/Stored Procedure Definition.sql
+++ Scripts/Stored Procedure Definition.sql
@@ -7,5 +7,5 @@ See also sp_helptext.
 
 SELECT  o.name, o.type_desc, o.create_date, o.modify_date, m.*
 FROM    sys.sql_modules m LEFT OUTER JOIN
-        sys.all_objects o on m.object_id = o.object_id
-WHERE   m.definition like  '%myText%';
+        sys.all_objects o ON m.object_id = o.object_id
+WHERE   m.definition LIKE  '%myText%';
